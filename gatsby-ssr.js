50108077dae6569785de8a411bacbd518c39f9f6
Laurent Harel <lrt.hrl@gmail.com>
2024-09-29 21:30:01 -0400

    :art: style: Use landing layout

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -14,14 +14,14 @@ import { ModalProvider } from './src/contexts/ModalContext';
 // import { ProgramFiltersProvider } from './src/contexts/ProgramFiltersContext';
 
 // components
-import Layout from './src/components/Layout/Layout';
+import LandingLayout from './src/components/Landing/Layout';
 import LanguageProvider from './src/contexts/LanguageContext/LanguageContext';
 
 // Apply Layout component to every page and template
 export const wrapPageElement = ({ element, props }) => (
   // props provide same data to Layout as Page element will get
   // including location, data, etc - you don't need to pass it
-  <Layout {...props}>{element}</Layout>
+  <LandingLayout {...props}>{element}</LandingLayout>
 );
 
 export const wrapRootElement = ({ element }) => (
