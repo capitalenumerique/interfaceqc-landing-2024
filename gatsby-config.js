f3010857db88e5a4f4fd41064757df0d439d4248
Krystina Hamel <35279620+KrystinaH@users.noreply.github.com>
2025-01-13 13:03:28 -0500

    :sparkles: feat: Add meta og and new partner (#7)
    Co-authored-by: Krystina Hamel <krystina.hamel@libeo.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,9 +7,10 @@ const inProduction = process.env.NODE_ENV === 'production';
 module.exports = {
   siteMetadata: {
     title: 'Interface',
-    description: '',
+    description:
+      'Interface - Québec, l’événement de formation par excellence pour les créateurs et professionnels du numérique et du web, créé par et pour la communauté. C’est à Québec du 27 au 29 mai 2025.',
     siteUrl: 'https://interfaceqc.org',
-    image: '',
+    image: '/og-img-interface.png',
   },
   plugins: [
     {
@@ -82,24 +83,24 @@ module.exports = {
       options: { paths: ['!/'] },
     },
     `gatsby-plugin-sitemap`,
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: 'Interface Québec',
-    //     short_name: 'IQC',
-    //     start_url: '/',
-    //     background_color: '#000CA0',
-    //     theme_color: '#000CA0',
-    //     // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
-    //     // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-    //     display: 'standalone',
-    //     icon: 'src/images/icon-waq21.png', // This path is relative to the root of the site.
-    //     // An optional attribute which provides support for CORS check.
-    //     // If you do not provide a crossOrigin option, it will skip CORS for manifest.
-    //     // Any invalid keyword or empty string defaults to `anonymous`
-    //     crossOrigin: `use-credentials`,
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'Interface Québec',
+        short_name: 'IQC',
+        start_url: '/',
+        background_color: '#F3EEE7',
+        theme_color: '#F3EEE7',
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: 'standalone',
+        icon: 'src/images/favicon.png', // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
+    },
     // 'gatsby-plugin-offline',
     // 'gatsby-plugin-netlify',
     // `gatsby-plugin-graphql-loader`,
